183c101358121f326980610799c02d44d88f5e1a
surkanghyun <qorrnfn2000@naver.com>
2024-05-27 14:16:24 +0900

    commit

---
+++ src/main/java/com/example/todo/todoapi/api/TodoController.java
@@ -0,0 +1,135 @@
+package com.example.todo.todoapi.api;
+
+import com.example.todo.auth.TokenUserInfo;
+import com.example.todo.todoapi.dto.request.TodoCreateRequestDTO;
+import com.example.todo.todoapi.dto.request.TodoModifyRequestDTO;
+import com.example.todo.todoapi.dto.response.TodoListResponseDTO;
+import com.example.todo.todoapi.service.TodoService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/api/todos")
+public class TodoController {
+
+    private final TodoService todoService;
+
+    // 할 일 등록 요청
+    @PostMapping
+    public ResponseEntity<?> createTodo(
+            // JwtAuthFilter에서 security에게 전역적으로 사용할 수 있는 인증 정보를 등록해 놓았기 때문에
+            // @AuthenticationPrincipal을 통해 토큰에 인증된 사용자 정보를 불러올 수 있다.
+            @AuthenticationPrincipal TokenUserInfo userInfo,
+            @Validated @RequestBody TodoCreateRequestDTO requestDTO,
+            BindingResult result
+    ) {
+        log.info("/api/todos GET! - dto: {}", requestDTO);
+        log.info("TokenUserInfo: {}", userInfo);
+        ResponseEntity<List<FieldError>> validatedResult = getValidatedResult(result);
+        if (validatedResult != null) return validatedResult;
+
+        TodoListResponseDTO responseDTO = todoService.create(requestDTO, userInfo.getUserId());
+        return ResponseEntity.ok().body(responseDTO);
+    }
+
+    // 할 일 목록 요청
+    @GetMapping
+    public ResponseEntity<?> retrieveTodoList(
+            @AuthenticationPrincipal TokenUserInfo userInfo
+    ) {
+        log.info("/api/todos GET request!");
+        try {
+            TodoListResponseDTO responseDTO = todoService.retrieve(userInfo.getUserId());
+            return ResponseEntity.ok().body(responseDTO);
+        } catch (Exception e) {
+            return ResponseEntity
+                    .internalServerError()
+                    .body(TodoListResponseDTO.builder()
+                            .error(e.getMessage())
+                            .build());
+        }
+    }
+
+    // 할 일 삭제 요청
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteTodo(
+            @AuthenticationPrincipal TokenUserInfo userInfo,
+            @PathVariable("id") String todoId
+    ) {
+        log.info("/api/todos/{} DELETE request!", todoId);
+
+        if (todoId == null || todoId.trim().equals("")) {
+            return ResponseEntity.badRequest()
+                    .body("ID를 전달해 주세요!");
+        }
+
+        try {
+            TodoListResponseDTO responseDTO = todoService.delete(todoId, userInfo.getUserId());
+            return ResponseEntity.ok().body(responseDTO);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    // 할 일 수정하기
+    @PatchMapping
+    public ResponseEntity<?> updateTodo(
+            @AuthenticationPrincipal TokenUserInfo userInfo,
+            @Validated @RequestBody TodoModifyRequestDTO requestDTO,
+            BindingResult result
+    ) {
+        ResponseEntity<List<FieldError>> validatedResult = getValidatedResult(result);
+        if(validatedResult != null) return validatedResult;
+
+        try {
+            return ResponseEntity.ok().body(todoService.update(requestDTO, userInfo.getUserId()));
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError()
+                    .body(e.getMessage());
+        }
+
+    }
+
+
+
+    // 입력값 검증(Validation)의 결과를 처리해 주는 전역 메서드
+    private static ResponseEntity<List<FieldError>> getValidatedResult(BindingResult result) {
+        if (result.hasErrors()) { // 입력값 검증 단계에서 문제가 있었다면 true
+            List<FieldError> fieldErrors = result.getFieldErrors();
+            fieldErrors.forEach(err -> {
+                log.warn("invalid client data - {}", err.toString());
+            });
+            return ResponseEntity
+                    .badRequest()
+                    .body(fieldErrors);
+        }
+        return null;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
